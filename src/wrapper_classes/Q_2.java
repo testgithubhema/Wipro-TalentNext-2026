1761b48e37d1c274beae4c165640d9ce63f4fa29
Hemasri <jangahemasri08@gmail.com>
2026-07-10 17:31:25 +0530

    Added Wrapper Classes Assignment

---
+++ src/wrapper_classes/Q_2.java
@@ -0,0 +1,13 @@
+package wrapper_classes;
+
+public class Q_2 {
+
+	public static void main(String[] args) {
+		int number = Integer.valueOf(args[0]);
+		System.out.println("Given Number : "+number);
+		System.out.println("Binary equivalent :"+Integer.toBinaryString(number));
+		System.out.println("Binary equivalent :"+Integer.toOctalString(number));
+		System.out.println("Binary equivalent :"+Integer.toHexString(number));
+	}
+
+}
---
+++ src/wrapper_classes/Q_3.java
@@ -0,0 +1,18 @@
+package wrapper_classes;
+import java.util.Scanner;
+public class Q_3 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter number : ");
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		String output = String.format("%8s", Integer.toBinaryString(input)).replace(' ', '0');
+		System.out.println(output);
+		
+		
+		sc.close();
+
+	}
+
+}
