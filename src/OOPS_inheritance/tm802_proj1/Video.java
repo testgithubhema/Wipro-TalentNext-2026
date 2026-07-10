7dc620cba5d45de3f26282d491c9067957fac9a2
Hemasri <jangahemasri08@gmail.com>
2026-07-10 20:15:00 +0530

    Added Video Rental Inventory System

---
+++ src/OOPS_inheritance/tm802_proj1/Video.java
@@ -0,0 +1,36 @@
+package OOPS_inheritance.tm802_proj1;
+public class Video {
+	
+	String videoName;
+	boolean checkout;
+	int rating;
+	
+	Video(String name){
+		videoName=name;
+	}
+	
+	String getName() {
+		return videoName;
+	}
+	
+	void doCheckout() {
+		checkout = true;
+	}
+	
+	void doReturn() {
+		checkout = false;
+	}
+	
+	void receiveRating(int rating) {
+		this.rating = rating;
+	}
+	
+	int getRating() {
+		return rating;
+	}
+	
+	boolean getCheckout() {
+		return checkout;
+	}
+	
+}
