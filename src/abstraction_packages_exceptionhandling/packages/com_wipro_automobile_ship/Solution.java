f204e65464352dea3562941626655bcb421c7608
Hemasri <jangahemasri08@gmail.com>
2026-07-10 07:37:31 +0530

    Added Packages Assignment

---
+++ src/abstraction_packages_exceptionhandling/packages/com_wipro_automobile_ship/Solution.java
@@ -0,0 +1,11 @@
+package abstraction_packages_exceptionhandling.packages.com_wipro_automobile_ship;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Compartment compartment = new Compartment(1005.15, 400.5, 588.2);
+		
+		System.out.println(compartment);
+	}
+
+}
