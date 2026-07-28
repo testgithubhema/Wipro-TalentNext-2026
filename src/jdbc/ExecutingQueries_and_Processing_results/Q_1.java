15961def67f08e1a206a3bfaf54c64ee200b9574
Hemasri <jangahemasri08@gmail.com>
2026-07-28 10:14:40 +0530

    Added JDBC  Assignments

---
+++ src/jdbc/ExecutingQueries_and_Processing_results/Q_1.java
@@ -0,0 +1,45 @@
+package jdbc.ExecutingQueries_and_Processing_results;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Q_1 {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+        String username = "hr";
+        String password = "hr";
+
+        try {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM EMP");
+
+            System.out.println("ID\tLAST_NAME");
+
+            while(rs.next()) {
+
+                int id = rs.getInt(1);                    // Column Index
+                String lname = rs.getString("LAST_NAME"); // Column Name
+
+                System.out.println(id + "\t" + lname);
+            }
+
+            rs.close();
+            stmt.close();
+            con.close();
+
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
