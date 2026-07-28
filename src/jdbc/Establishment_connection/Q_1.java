1895b44133a2229d25e10924d84f37f5a06e6df6
Hemasri <jangahemasri08@gmail.com>
2026-07-28 09:34:06 +0530

    Added JDBC Connection Establishmnet Assignments

---
+++ src/jdbc/Establishment_connection/Q_1.java
@@ -0,0 +1,38 @@
+package jdbc.Establishment_connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Q_1 {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+        String username = "hr";
+        String password = "hr";
+
+        try {
+
+          
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+     
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            if (con != null) {
+                System.out.println("Connection Established successfully");
+            }
+
+        
+            con.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Connection could not be established");
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+
+}
